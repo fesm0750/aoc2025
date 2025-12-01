e67513181ad1bf5866732cd67471cd05fb2debd4
Fernando Martins <emmanuel.martins@outlook.com>
2025-12-01 10:48:47 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use aoc2025::*;
+use std::env;
+
+fn main() {
+    let input = env::args().nth(1);
+    if input.is_none() {
+        println!("No input argument.");
+        return;
+    }
+
+    match input.unwrap().parse().unwrap() {
+        1 => day01::run(),
+        _ => println!("Invalid input argument."),
+    }
+}
