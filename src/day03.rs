c1356f86c316f42473ceb3f8bea71a7756fa7667
Fernando Martins <emmanuel.martins@outlook.com>
2025-12-04 14:36:48 -0300

    refactor(Day03): use const generic and add tests - use const generic for array size - add test

--- src/day03.rs
+++ src/day03.rs
@@ -4,16 +4,15 @@
 use std::fs;
 
 const BANK_SIZE: usize = 100;
-type BatteryBank = [u8; BANK_SIZE];
 
 pub fn run() {
     let input = fs::read_to_string("inputs/day03").unwrap();
-    let banks = parse_input(&input);
+    let banks = parse_input::<BANK_SIZE>(&input);
 
     println!("answer pt1: {}", pt1(&banks));
 }
 
-fn parse_input(s: &str) -> Vec<BatteryBank> {
+fn parse_input<const N: usize>(s: &str) -> Vec<[u8; N]> {
     s.lines()
         .map(|line| {
             line.chars()
@@ -26,12 +25,12 @@ fn parse_input(s: &str) -> Vec<BatteryBank> {
         .collect()
 }
 
-fn pt1(banks: &[BatteryBank]) -> u64 {
+fn pt1<const N: usize>(banks: &[[u8; N]]) -> u64 {
     let mut total_joltage = 0;
     for bank in banks {
         let (mut n1, mut n0) = (0, 0);
 
-        for &b in &bank[0..BANK_SIZE - 1] {
+        for &b in &bank[0..N - 1] {
             n0 = n0.max(b);
             if n0 > n1 {
                 n1 = n0;
@@ -39,9 +38,26 @@ fn pt1(banks: &[BatteryBank]) -> u64 {
             }
         }
 
-        n0 = n0.max(bank[BANK_SIZE - 1]);
+        n0 = n0.max(bank[N - 1]);
         total_joltage += (n1 * 10 + n0) as u64;
     }
 
     total_joltage
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+    #[test]
+    fn test_pt1() {
+        let ranges = parse_input::<15>(INPUT);
+        let total = pt1(&ranges);
+        assert_eq!(total, 357);
+    }
+    }
