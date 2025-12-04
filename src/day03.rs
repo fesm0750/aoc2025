86458aa026455a07779e2ae4708095713ddd0471
Fernando Martins <emmanuel.martins@outlook.com>
2025-12-04 14:37:20 -0300

    feat(Day03): add pt2 solution

--- src/day03.rs
+++ src/day03.rs
@@ -10,6 +10,7 @@ pub fn run() {
     let banks = parse_input::<BANK_SIZE>(&input);
 
     println!("answer pt1: {}", pt1(&banks));
+    println!("answer pt2: {}", pt2(&banks));
 }
 
 fn parse_input<const N: usize>(s: &str) -> Vec<[u8; N]> {
@@ -45,6 +46,40 @@ fn pt1<const N: usize>(banks: &[[u8; N]]) -> u64 {
     total_joltage
 }
 
+fn pt2<const N: usize>(banks: &[[u8; N]]) -> u64 {
+    const SIZE: usize = 12;
+    let mut total_joltage = 0;
+
+    for bank in banks {
+        let mut joltage = [0u8; SIZE];
+        joltage.copy_from_slice(&bank[N - SIZE..N]);
+
+        for &curr in bank[0..N - SIZE].iter().rev() {
+            let mut aux = curr;
+            for b in joltage.iter_mut() {
+                if aux >= *b {
+                    std::mem::swap(&mut *b, &mut aux);
+                } else if aux == *b {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        let joltage = joltage
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(i, &d)| d as u64 * 10u64.pow(i as u32))
+            .sum::<u64>();
+
+        total_joltage += joltage;
+    }
+
+    total_joltage
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -60,4 +95,11 @@ mod tests {
         let total = pt1(&ranges);
         assert_eq!(total, 357);
     }
+
+    #[test]
+    fn test_pt2() {
+        let ranges = parse_input::<15>(INPUT);
+        let total = pt2(&ranges);
+        assert_eq!(total, 3121910778619);
     }
+}
