13e2a7b7ac73ef29bd2a114635b2742f09c376b5
Fernando Martins <emmanuel.martins@outlook.com>
2025-12-08 01:26:54 -0300

    feat(Day07): add pt2 and refactor - move parse to a new fn; - modify Manifold::Beam to hold a u64 value, this will be used to keep track of the timelines; - remove derive PartialEq from Manifold, using if let to compare instead; - refactor fn count_splits to adjust to the aforementioned and better readability; - modify fn `shoot_beam` to update the amount of overlapped beams (timelines); - create fn count_timelines for pt2; - add test.

--- src/day07.rs
+++ src/day07.rs
@@ -8,26 +8,43 @@ use aoc_tools::grid::Grid;
 
 pub fn run() {
     let input = fs::read_to_string("inputs/day07").unwrap();
-    let len_x = input.split_once('\n').unwrap().0.len();
-
-    let vec: Vec<Manifold> = input.chars().flat_map(Manifold::from_char).collect();
-    let mut field = Grid::with_borders(len_x, Empty, 1, vec);
+    let mut field = parse_input(&input);
 
     shoot_beam(&mut field);
     println!("answer pt1: {}", count_splits(&field));
+    println!("answer pt2: {}", count_timelines(&field));
+}
+
+fn parse_input(input: &str) -> Grid<Manifold> {
+    let len_x = input.split_once('\n').unwrap().0.len();
+    let vec: Vec<Manifold> = input.chars().flat_map(Manifold::from_char).collect();
+    Grid::with_borders(len_x, Empty, 1, vec)
 }
 
 fn shoot_beam(field: &mut Grid<Manifold>) {
-    for y in 1..field.len_y - 1 {
-        for x in 1..field.len_x - 1 {
-            let curr = field[(x, y)];
-            let below = field[(x, y + 1)];
-            match (curr, below) {
-                (Start, _) => *field.get_mut(x, y + 1) = Beam,
-                (Beam, Empty) => *field.get_mut(x, y + 1) = Beam,
-                (Beam, Splitter) => {
-                    *field.get_mut(x - 1, y + 1) = Beam;
-                    *field.get_mut(x + 1, y + 1) = Beam;
+    for row in 1..field.len_y - 1 {
+        for col in 1..field.len_x - 1 {
+            let curr = field[(col, row)];
+            let below = field.get_mut(col, row + 1);
+
+            let update = |upd: &mut Manifold, n: u64| {
+                if let Beam(m) = upd {
+                    *upd = Beam(n + *m);
+                } else {
+                    *upd = Beam(n);
+                }
+            };
+
+            match (curr, *below) {
+                (Start, _) => *below = Beam(1),
+                (Beam(n), Empty) => update(below, n),
+                (Beam(n), Beam(_)) => update(below, n),
+                (Beam(n), Splitter) => {
+                    let left = field.get_mut(col - 1, row + 2);
+                    update(left, n);
+
+                    let right = field.get_mut(col + 1, row + 2);
+                    update(right, n);
                 }
                 _ => (),
             }
@@ -36,20 +53,32 @@ fn shoot_beam(field: &mut Grid<Manifold>) {
 }
 
 fn count_splits(field: &Grid<Manifold>) -> u64 {
-    let mut total = 0;
-    for x in 1..field.len_x {
-        for y in 1..field.len_y {
-            if field[(x, y)] == Splitter && field[(x, y - 1)] == Beam {
-                total += 1;
+    let mut splits = 0;
+    for row in 1..field.len_y - 1 {
+        for col in 1..field.len_x - 1 {
+            let (curr, above) = (field[(col, row)], field[(col, row - 1)]);
+            if let Splitter = curr
+                && let Beam(_) = above
+            {
+                splits += 1;
             }
         }
     }
-    total
+    splits
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+fn count_timelines(field: &Grid<Manifold>) -> u64 {
+    let last_row = field.len_y - 1;
+    let last_row = field.row(last_row);
+    last_row
+        .iter()
+        .map(|item| if let &Beam(n) = item { n } else { 0 })
+        .sum()
+}
+
+#[derive(Debug, Clone, Copy)]
 enum Manifold {
-    Beam,
+    Beam(u64),
     Empty,
     Splitter,
     Start,
@@ -61,8 +90,37 @@ impl Manifold {
             '.' => Empty,
             '^' => Splitter,
             'S' => Start,
-            '|' => Beam,
+            '|' => Beam(0),
             _ => None?,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+        let mut field = parse_input(input);
+        shoot_beam(&mut field);
+        assert_eq!(21, count_splits(&field));
+        assert_eq!(40, count_timelines(&field));
     }
 }
